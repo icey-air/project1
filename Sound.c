ab602baec184bc7040defdd03780db1d195077d8
WangGang <13150191666@163.com>
2026-03-27 16:15:13 +0800

    Passenger_GUI

---
+++ Sound.c
@@ -0,0 +1,13 @@
+#include "Sound.h"
+
+
+
+/*@breif    播放按钮音效
+* @param    无
+* @return	无
+*/
+void Play_Button_Sound()
+{
+    // PlaySound(TEXT("Sound\\Button.wav"), NULL, SND_FILENAME | SND_ASYNC);
+}
+
---
+++ Sound.h
@@ -0,0 +1,12 @@
+#ifndef __SOUND_H__
+#define __SOUND_H__
+
+#include <stdio.h>
+#include <windows.h>
+
+
+void Play_Button_Sound();
+
+
+
+#endif 
